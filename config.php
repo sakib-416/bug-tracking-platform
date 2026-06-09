a456d9c078aa982b8f3eefebc26d8ea1cdf6fdb2
Shariful Islam Sakib <22235103416@cse.bubt.edu.bd>
2026-06-09 10:31:10 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $db_user = 'sakib'; 
-$db_pass = 'password_here'; // তোর ডাটাবেজের পাসওয়ার্ড এখানে দিবি
+$db_pass = 'sakib';ি
 $db_name = 'sqli';
 
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
